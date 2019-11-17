f878f5f5f682b0a96fa7929d4e11692b7e6bbaba
lizfaria <efaria93@gmail.com>
2019-11-17 11:18:28 -0500

    merged new-cohort application

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { request } from "./backend-request";
+import Dashboard from "./components/Dashboard"
 
 function App() {
   useEffect(() => {
@@ -12,20 +13,7 @@ function App() {
   });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Dashboard />
     </div>
   );
 }
