0e5488674db3099246e44e8520013c94b03ffdb9
Anna Ionova <echoespond@gmail.com>
2019-11-21 20:18:16 -0500

    Really add package-lock.json

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { store } from "./redux/Store";
+import { store } from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
