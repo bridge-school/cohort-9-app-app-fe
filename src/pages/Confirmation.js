3094b1c6729085913adbc4d2b71aa377c7c047c7
lizfaria <efaria93@gmail.com>
2019-11-26 21:58:39 -0500

    tests

--- src/pages/Confirmation.js
+++ src/pages/Confirmation.js
@@ -3,31 +3,34 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { fetchAllApps } from "../redux/actions/fetchApps";
 
-// fetch specific cohort information that relates to form just filled out (fetch by id? by name?)
-// cohort name
-// close date
-// date of response
-const Confirmation = ({apps, getAllApps}) => {  
-    useEffect(() => {
+// for this to work, in student data, we need to include the apps data id they are applying to
+
+const Confirmation = ({apps, getAllApps, appId}) => {  
+  useEffect(() => {
         getAllApps();
     }, [getAllApps]);
-
-    const {cohortName} = apps.apps.cohort_apps;
-    
-    
-  return (
-    <>
-        {apps.apps.cohort_apps && 
-         <h1>  
-            `tada 
-            Congratulations! You have successfully submitted your application to ${cohortName}. 
-            Applications will be open until Close Date. 
-            All applicants will hear back from the Bridge team by Date of Response. 
-            tada`
-        </h1>
-        }
-    </>
-  )
+    console.log(apps.apps.cohort_apps)
+return (
+  <>
+  {apps.apps.cohort_apps &&
+  apps.apps.cohort_apps
+    .filter(app => app.id === "QlfCwZcCu4O5gKnhUoam")
+    .map(app => {
+      // also need to destructure close data and date of response to include in text
+      const {cohortName} = app;
+      return (
+        <h1>  
+          `🎉
+          Congratulations! You have successfully submitted your application to {cohortName}. 
+          Applications will be open until Close Date. 
+          All applicants will hear back from the Bridge team by Date of Response. 
+          🎉`
+      </h1>
+      )
+    })
+  }
+  </>
+)
 }
 const mapStateToProps = state => ({
     isLoading: state.isLoading,
@@ -36,4 +39,5 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
   getAllApps: () => dispatch(fetchAllApps()),
 });
+
 export default connect(mapStateToProps, mapDispatchToProps)(Confirmation);
