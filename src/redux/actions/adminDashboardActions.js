3febc301311d295cdc918c50a6ece03ff58565b4
sogol <sogol.naseri@gmail.com>
2019-11-30 11:04:20 -0500

    changed action and reducer names

--- src/redux/actions/adminDashboardActions.js
+++ src/redux/actions/adminDashboardActions.js
@@ -2,14 +2,13 @@ import { request } from "../../backend-request";
 
 export const ACTION_TYPES = {
   SET_APPS: "SET_APPS",
-  SET_IS_LOADING:"SET_IS_LOADING"    
-}
+  SET_IS_LOADING: "SET_IS_LOADING"
+};
 
 export const setApps = (apps = []) => ({
   type: ACTION_TYPES.SET_APPS,
   payload: apps
 });
-
 
 export const setIsLoadingAllApps = (isLoading = false) => ({
   type: ACTION_TYPES.SET_IS_LOADING,
@@ -18,9 +17,8 @@ export const setIsLoadingAllApps = (isLoading = false) => ({
 
 // fetchAllAppsThunk
 export const fetchAllApps = () => async dispatch => {
-    const res = await request("applications")
-    const apps = await res.json();
-    await dispatch(setApps(apps));
-    await dispatch(setIsLoadingAllApps(false));
+  const res = await request("applications");
+  const apps = await res.json();
+  await dispatch(setApps(apps));
+  await dispatch(setIsLoadingAllApps(false));
 };
-
