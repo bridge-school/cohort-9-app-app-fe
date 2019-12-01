d338f1d2d93e3c06ff5275516e0b58f2761fa564
lizfaria <efaria93@gmail.com>
2019-12-01 12:03:06 -0500

    created reducer test for getapps reducer

---
+++ src/tests/reducers/adminDashboardReducer.spec.js
@@ -0,0 +1,43 @@
+import {adminDashboardReducer} from "../../redux/reducers/adminDashboardReducer"
+import {ACTION_TYPES} from "../../redux/actions/adminDashboardActions"
+
+const {SET_APPS, SET_IS_LOADING} = ACTION_TYPES;
+
+const reducer = adminDashboardReducer;
+const INITIAL_STATE = {
+    apps: {
+      cohort_apps: []
+    },
+    isLoading: true
+};
+
+describe("Get Apps Reducer", () => {
+    it("returns initial state", () => {
+        expect(reducer(undefined, {})).toEqual(INITIAL_STATE);
+    });
+    describe("When fetchAllApps thunk is dispatched", () => {
+        it("updates the apps with the result json", () => {
+          const setApps = {
+            type: SET_APPS,
+            payload: { cohort_apps: [ {id: "status", value: ["all"]}]}
+          };
+          const expectedState = {
+            apps: { cohort_apps: [ {id: "status", value: ["all"]}]},
+            isLoading: true
+          };
+          expect(reducer(undefined, setApps)).toEqual(expectedState);
+        });
+        it("updates loading state to false", () => {
+            const setIsLoadingAllApps = {
+              type: SET_IS_LOADING,
+              payload: false
+            };
+            const expectedState = {
+              ...INITIAL_STATE,
+              isLoading: false
+            };
+            expect(reducer(undefined, setIsLoadingAllApps)).toEqual(expectedState);
+          });
+    });
+});
+
