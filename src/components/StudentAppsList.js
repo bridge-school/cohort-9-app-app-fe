7af34837ab8c80930500a77cbca01b13a55204a0
sogol <sogol.naseri@gmail.com>
2019-11-30 22:48:34 -0500

    added filter for students dates

--- src/components/StudentAppsList.js
+++ src/components/StudentAppsList.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import AppListItem from "./AppListItem.js"
-import {AppListStyled} from "./AppListStyled"
+import React from "react";
+import AppListItem from "./AppListItem.js";
+import { AppListStyled } from "./AppListStyled";
+import moment from "moment";
 
-const AppsList = ({apps}) => { 
-    return (
-        <>
-        <AppListStyled> 
-            {apps.map(li => {
-                const {id, cohortName, cohortType} = li
-                return (
-                 <AppListItem 
-                    key={id}
-                    cohortName={cohortName}
-                    cohortType={cohortType}
-                    
-                 />
-                )
-            })}     
-        </AppListStyled>
-        </>
-    )
-}
-   
-export default AppsList;
+const StudentsAppsList = ({ apps }) => {
+  const getFilteredCohort = ({ apps }) => {
+    return apps.filter(app => {
+      const dateClose = moment(app.dateClose, "YYYY-MM-DDTHH:mm:ss.SSSZ");
+      const now = moment();
+      console.log("now is " + now);
+      console.log("dateLimit is " + dateClose);
+      return dateClose.isValid() && now.isBefore(dateClose);
+    });
+  };
+  return (
+    <>
+      <AppListStyled>
+        {getFilteredCohort({ apps }).map(li => {
+          const { id, cohortName, cohortType, link } = li;
+          return (
+            <AppListItem
+              key={id}
+              cohortName={cohortName}
+              cohortType={cohortType}
+              link={link}
+            />
+          );
+        })}
+      </AppListStyled>
+    </>
+  );
+};
+
+export default StudentsAppsList;
