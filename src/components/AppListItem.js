13cdebde38ff1f604010d77df44ec13fbfa02d48
lizfaria <efaria93@gmail.com>
2019-12-02 11:53:23 -0500

    added key to li in applistitem component

--- src/components/AppListItem.js
+++ src/components/AppListItem.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const AppListItem = ({key, cohortName, cohortType, link}) => {
     return (
-        <Segment as="li" clearing padded>
+        <Segment as="li" clearing padded key={key}>
             <Header as='h2' floated='left'>{cohortName}</Header>
             <Button  floated='right' as={Link} link={link} color={cohortType==="Backend Development" ? "blue" : cohortType==="Frontend Development" ? "pink" : "teal"}>
                 {cohortType}
