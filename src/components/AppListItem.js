27b8f547dfd913ca08e27c2e7e24ee25acbe9beb
lizfaria <efaria93@gmail.com>
2019-12-04 21:06:54 -0500

    unit test passing now - had to add data attributes to semantic ui elements on AppList component to be able to test them. Also changed test so only testing container one level deep in description.

--- src/components/AppListItem.js
+++ src/components/AppListItem.js
@@ -3,15 +3,16 @@ import { Button, Segment, Header} from 'semantic-ui-react'
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const AppListItem = ({key, cohortName, cohortType, link}) => {
-    
     return (
         cohortName && (
-            <Segment as="li" clearing padded key={key}>
-                <Header as='h2' floated='left'>{cohortName}</Header>
-                <Button  floated='right' as="Link" href={link} color={cohortType==="Backend Development" ? "blue" : cohortType==="Frontend Development" ? "pink" : "teal"}>
-                    {cohortType}
-                </Button>
-            </Segment>
+            <Segment clearing padded as="li" key={key} data_test="appListItem_Segment">
+                <Link to={link}>
+                    <Header as='h2' floated='left'>{cohortName}</Header>
+                    <Button data_test="appListItem_Button" floated='right' color={cohortType==="Backend Development" ? "blue" : cohortType==="Frontend Development" ? "pink" : "teal"}>
+                        {cohortType}
+                    </Button>
+                </Link>
+            </Segment>    
         )
     )
 }
--- src/tests/components/AppListItem.test.js
+++ src/tests/components/AppListItem.test.js
@@ -1,39 +1,92 @@
 import React from "react";
+import { act, isElementOfType, Simulate} from "react-dom/test-utils";
+import AppListItem from "../../components/AppListItem";
+import { MemoryRouter, Link } from 'react-router-dom';
+import { Router } from 'react-router';
 import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
-import AppListItem from "../../components/AppListItem.js";
-
-
-let container = null;
-beforeEach(() => {
-  // setup a DOM element as a render target
-  container = document.createElement("div");
-  document.body.appendChild(container);
-});
-afterEach(() => {
-  // cleanup on exiting
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
-
+import { createMemoryHistory } from "history";
 describe("Applications List Item", () => {
     describe("displays with props", () => {
-        it("renders with or without data", () => {
-        act(() => {
-            render(<AppListItem />, container);
+        let container = null;
+        beforeEach(() => {
+            // setup a DOM element as a render target
+            container = document.createElement("div");
+            document.body.appendChild(container);
         });
-        expect(container.querySelector("p").textContent).toBe("");
-        expect(container.querySelector("a").textContent).toBe("");
-        expect(container.textContent).toContain("");
-
-        act(() => {
-            render(<AppListItem  key="10" cohortName="test_name" cohortType="test_type" link="/" 
-                />, container);
+        afterEach(() => {
+            // cleanup on exiting
+            unmountComponentAtNode(container);
+            container.remove();
+            container = null;
         });
-        expect(container.querySelector("p").textContent).toBe("test_name");
-        expect(container.querySelector("a").textContent).toBe("test_type");
-        expect(container.querySelector("a").attributes.getNamedItem('href').value).toBe("/");
+        it("should not render when a cohortName is empty", () => {
+            const mockCohortName = ""
+            act(() => {
+                render(
+                    <MemoryRouter>
+                        <AppListItem cohortName={mockCohortName} link=""/>
+                    </MemoryRouter>, container);
+                expect(container.querySelector('[data_test="appListItem_Segment"]')).toBeNull()
+            });
+        });
+        it("should render when a cohortName is passed", () => {
+            const mockCohortName = "test"
+            act(() => {
+                render(
+                    <MemoryRouter>
+                        <AppListItem cohortName={mockCohortName} link=""/>
+                    </MemoryRouter>, container)
+            })
+            expect(container.querySelector('[data_test="appListItem_Segment"]')).toBeTruthy()
+            expect(container.querySelector('[data_test="appListItem_Segment"]').nodeName).toBe('LI')
+            expect(container.querySelector('h2').textContent).toBe(mockCohortName);
+            expect(container.querySelector('[data_test="appListItem_Button"]').textContent).toBe('');
+        });
+        it("should render the correct text when a cohortName and cohortType are passed", () => {
+            const mockCohortName = "test"
+            const mockCohortType = "Frontend Development"
+            act(() => {
+                render(
+                    <MemoryRouter>
+                        <AppListItem cohortName={mockCohortName} cohortType={mockCohortType} link=""/>
+                    </MemoryRouter>
+                , container)
+            })
+            expect(container.querySelector('h2').textContent).toBe(mockCohortName);
+            expect(container.querySelector('[data_test="appListItem_Button"]').textContent).toBe(mockCohortType);
+        });
+        it(`should render the correct color for the data_test="appListItem_Button" when a cohortType is passed`, () => {
+            const mockCohortName = "test"
+            const mockCohortTypeFE = "Frontend Development"
+            act(() => {
+                render(
+                    <MemoryRouter>
+                        <AppListItem cohortName={mockCohortName} cohortType={mockCohortTypeFE} link=""/>
+                    </MemoryRouter>, container)
+            })
+            expect(container.querySelector('[data_test="appListItem_Button"]').className).toEqual(expect.stringContaining("pink"))
+        });
+        it(`should render the correct color for the data_test="appListItem_Button" when a cohortType is passed`, () => {
+            const mockCohortName = "test"
+            const mockCohortTypeBE = "Backend Development"
+            act(() => {
+                render(
+                    <MemoryRouter>
+                        <AppListItem cohortName={mockCohortName} cohortType={mockCohortTypeBE} link=""/>
+                    </MemoryRouter>, container)
+            })
+            expect(container.querySelector('[data_test="appListItem_Button"]').className).toEqual(expect.stringContaining("blue"))
+        });
+        it(`should render the correct color for the data_test="appListItem_Button" when a cohortType is passed`, () => {
+            const mockCohortName = "test"
+            const mockCohortType = ""
+            act(() => {
+                render(
+                    <MemoryRouter>
+                        <AppListItem cohortName={mockCohortName} cohortType={mockCohortType} link=""/>
+                    </MemoryRouter>, container)
+            })
+            expect(container.querySelector('[data_test="appListItem_Button"]').className).toEqual(expect.stringContaining("teal"))
         });
     });
 });
