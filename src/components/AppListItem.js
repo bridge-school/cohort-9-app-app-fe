b99b67038122393d50a4360cb76815820b41c71f
lizfaria <efaria93@gmail.com>
2019-12-05 19:42:36 -0500

    added :id to end of student-form route in app.js and removed exact attribute from route. Change link in studentapplist component to include id of cohort at end of path. Included params in studentApplication to accept id. mapped state of apps to component and filtered through it with id passed in through params to get only this cohorts data

--- src/components/AppListItem.js
+++ src/components/AppListItem.js
@@ -2,17 +2,21 @@ import React from 'react';
 import { Button, Segment, Header} from 'semantic-ui-react'
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-const AppListItem = ({key, cohortName, cohortType, link}) => {
+const AppListItem = ({id, cohortName, cohortType, link}) => { 
     return (
         cohortName && (
-            <Segment clearing padded as="li" key={key} data_test="appListItem_Segment">
+            <li key={id}>
                 <Link to={link}>
+                <Segment clearing padded data_test="appListItem_Segment">
                     <Header as='h2' floated='left'>{cohortName}</Header>
-                    <Button data_test="appListItem_Button" floated='right' color={cohortType==="Backend Development" ? "blue" : cohortType==="Frontend Development" ? "pink" : "teal"}>
+                    <Button data_test="appListItem_Button" 
+                            floated='right' 
+                            color={cohortType==="Backend Development" ? "blue" : cohortType==="Frontend Development" ? "pink" : "teal"}>
                         {cohortType}
                     </Button>
+                </Segment> 
                 </Link>
-            </Segment>    
+            </li>        
         )
     )
 }
