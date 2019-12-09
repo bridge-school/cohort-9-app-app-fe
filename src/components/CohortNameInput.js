384637de64e5851352dcd395f36dc1c9985a8d82
lizfaria <efaria93@gmail.com>
2019-12-09 13:56:26 -0500

    fixed conflicts due to renaming textInput component

--- src/components/CohortNameInput.js
+++ src/components/CohortNameInput.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-<<<<<<< HEAD:src/components/CohortNameInput.js
 import {
   InputField,
   InputLabel
@@ -8,11 +7,6 @@ import {
 
 
 const CohortNameInput = (props) => {
-=======
-import { InputField, InputLabel } from "./TextInputStyled";
-
-const TextInput = props => {
->>>>>>> 8cea5c9263055c49af59d179b675fc2484057e6f:src/components/TextInput.js
   return (
     <div>
       <InputLabel>
@@ -22,10 +16,7 @@ const TextInput = props => {
           value={props.value}
           onChange={props.handleChange}
           maxLength="64"
-<<<<<<< HEAD:src/components/CohortNameInput.js
-=======
           placeholder="Enter Cohort Name"
->>>>>>> 8cea5c9263055c49af59d179b675fc2484057e6f:src/components/TextInput.js
           required
         />
       </InputLabel>
@@ -33,8 +24,4 @@ const TextInput = props => {
   );
 };
 
-<<<<<<< HEAD:src/components/CohortNameInput.js
 export default CohortNameInput;
-=======
-export default TextInput;
->>>>>>> 8cea5c9263055c49af59d179b675fc2484057e6f:src/components/TextInput.js
