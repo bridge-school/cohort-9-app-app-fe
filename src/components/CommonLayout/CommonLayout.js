4a3095657eac6164f459df632fd5ccbcb7505d4a
Yena Park <parkyen@sheridancollege.ca>
2019-12-09 23:13:11 -0500

    add styling for header

--- src/components/CommonLayout/CommonLayout.js
+++ src/components/CommonLayout/CommonLayout.js
@@ -4,7 +4,7 @@ import {Wrapper} from './CommonLayoutStyled'
 const CommonLayout = ({children, isAdmin}) => {
   return (
     <div>
-      <Header isAdmin={isAdmin}/>
+      <Header isAdmin={isAdmin} title={'Cohort Application'}/>
       <Wrapper>
         {children}
       </Wrapper>
---
+++ src/components/Header/HeaderStyled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const NavStyled = styled.nav`
+  background: #4e5cc3;
+`
+
+export const H2Styled = styled.h2`
+  color: white;
+`
+
+export const AdminViewStyled = styled.div`
+  color: white;
+  font-size: 1.5rem;
+  padding-right: 2rem;
+`
