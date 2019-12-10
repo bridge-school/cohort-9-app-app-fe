e1d3b6bfad2a6a990a8d6fc67c21c6b9f0089e14
Yena Park <parkyen@sheridancollege.ca>
2019-12-09 23:20:49 -0500

    add styling for question and refactoring

--- src/components/Questions/Question.js
+++ src/components/Questions/Question.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Form, Checkbox } from 'semantic-ui-react'
 
 const Question = ({
   question,
@@ -10,84 +11,71 @@ const Question = ({
   onDelete
 }) => {
   const qNumber = index + 1;
-  const optionsString = question.options.join(", ");
+  const optionsString = question.options ? question.options.join(', ') : ''
+  const QUESTION_TYPE_OPTIONS = [
+    { key: 'short-answer', text: 'Short Answer', value: 'short-answer'},
+    { key: 'drop-down', text: 'Dropdown', value: 'drop-down'},
+    { key: 'check-box', text: 'Checkboxes', value: 'check-box'},
+    { key: 'paragraph', text: 'Paragraph', value: 'paragraph'}
+  ]
 
   return (
-    <>
-      <div>
-        <label htmlFor={`q${index}__name`}>
-          {`Question #${qNumber}`}
-          <input
-            id={`q${index}__name`}
-            type="text"
-            value={question.name}
-            onChange={e => {
-              const value = e.target.value;
-              onPromptChange(index, value);
-            }}
-            required
-          />
-        </label>
-      </div>
-      <div>
-        <label htmlFor={`q${index}__type`}>
-          {`Question #${qNumber} Type`}
-          <select
-            id={`q${index}__type`}
-            onChange={e => {
-              const value = e.target.value;
-              onTypeChange(index, value);
-            }}
-            value={question.type}
-            required
-          >
-            <option value="short-answer">Short Answer</option>
-            <option value="paragraph">Paragraph</option>
-            <option value="checkboxes">Checkboxes</option>
-            <option value="dropdown">Dropdown</option>
-          </select>
-        </label>
-      </div>
-      {question.type === "checkboxes" || question.type === "dropdown" ? (
-        <div>
-          <label htmlFor={`q${index}__options`}>
-            <input
-              id={`q${index}__options`}
-              type="text"
-              value={optionsString}
-              placeholder="array"
-              onChange={e => {
-                const value = e.target.value;
-                onOptionsChange(index, value);
-              }}
-              required
-            />
-          </label>
-        </div>
-      ) : null}
-      <div>
-        <label htmlFor={`q${index}__req`}>
-          Is Required
-          <input
-            id={`q${index}__req`}
-            defaultChecked={question.required}
-            onChange={() => {
-              onIsRequiredChange(index);
-            }}
-            type="checkbox"
-          />
-        </label>
-      </div>
-      <div>
-        <button
+    <Form>
+      <Form.Group>
+        <Form.Input
+          id={`q${index}__name`}
+          label={{ children: `Question #${qNumber}`, htmlFor: `q${index}__name` }}
+          onChange={(e, {value}) => {
+            onPromptChange(index, value);
+          }}
+          value={question.prompt}
+          width={8}
+        />
+        <Form.Select
+          fluid
+          id={`q${index}__type`}
+          label={{ children: `Question #${qNumber} Type`, htmlFor: `q${index}__type` }}
+          options={QUESTION_TYPE_OPTIONS}
+          onChange={(e, {value}) => {
+            onTypeChange(index, value);
+          }}
+          value={question.type}
+          width={4}
+        />
+        <Form.Field
+          id={`q${index}__req`}
+          control={Checkbox}
+          label={{ children: `Is Required`, htmlFor: `q${index}__req` }}
+          onChange={(e, {checked: value}) => {
+            onIsRequiredChange(index, value);
+          }}
+          checked={question.required}
+          width={2}
+        />
+        <Form.Button
+          content="X"
+          type="button"
           onClick={() => {
             onDelete(index);
           }}
-        >
-          Delete
-        </button>
-      </div>
-    </>
+          width={2}
+        />
+      </Form.Group>
+      {(question.type === "check-box" || question.type === "drop-down") &&
+        <Form.Group>
+          <Form.Input
+            id={`q${index}__options`}
+            label={{ children: `Question #${qNumber} type options`, htmlFor: `q${index}__options` }}
+            onChange={(e, {value}) => {
+              onOptionsChange(index, value);
+            }}
+            value={optionsString}
+            placeholder="a, b, c, d"
+            width={16}
+          />
+        </Form.Group>
+      }
+    </Form>
   );
 };
 
