e0b713b9cf33922dd7ad41ff091bda93d4cb521b
Anna Ionova <echoespond@gmail.com>
2019-12-10 09:05:57 -0500

    Minor change

--- src/components/CohortNameInput.js
+++ src/components/CohortNameInput.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 
 import {
   InputField,
@@ -21,5 +22,26 @@ const CohortNameInput = (props) => {
     </label>
   );
 };
+
+const InputField = styled.input`
+  width: 100%;
+  height: 36px;
+  margin-top: 10px;
+  border-radius: 8px;
+  border-color: #ccc;
+  background: #f4f4f4;
+  color: #333;
+  font-size: 16px;
+  padding-left: 10px;
+`;
+
+const InputLabel = styled.label`
+  display: block;
+  width: 100%;
+  text-align: left;
+  color: #1e1e1e;
+  font-weight: 700;
+  margin-bottom: 20px;
+`;
 
 export default CohortNameInput;
